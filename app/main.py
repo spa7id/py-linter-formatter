41af2fc5d9581efac1e949e6fe71db1a8b5f0007
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-09-18 16:54:23 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 def format_linter_error(error: dict) -> dict:
-    return {new_key: error[old_key] for new_key, old_key in {
-        "line": "line_number",
-        "column": "column_number",
-        "message": "text",
-        "name": "code"
-    }.items()} | {"source": "flake8"}
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
+
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
